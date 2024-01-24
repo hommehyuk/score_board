afd3da7563ccb0818fff1c310771b116939cdb83
hommehyuk <bin6827@gmail.com>
2024-01-24 16:43:09 +0900

    controller widget

---
+++ lib/presentation/widgets/controller_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+enum PlayerType { blue, red }
+
+const double DEFAULT_TIME = 15 * 60 * 1000;
+
+class ControllerProvider extends ChangeNotifier {
+  final List<int> _scoreList = List.filled(PlayerType.values.length, 0);
+  double remainingTimeInMs = DEFAULT_TIME;
+
+  void changePlayerScore(PlayerType player, int scoreVariance) {
+    _scoreList[player.index] += scoreVariance;
+    notifyListeners(); // Notify listeners of changes
+  }
+
+  int getPlayerScore(PlayerType playerType) {
+    return _scoreList[playerType.index];
+  }
+}
